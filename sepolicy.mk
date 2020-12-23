dea09b9d7944dd776d21aa0e1e4c711ebc34c1fb
Nicholas Magill <nmagill1979@gmail.com>
2020-12-23 13:03:39 +0000

    Initial andromeda bringup

---
+++ sepolicy.mk
@@ -0,0 +1,6 @@
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/xiaomi/andromeda-sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/xiaomi/andromeda-sepolicy/private
+
+# Vendor
+BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/andromeda-sepolicy/vendor/xiaomi
+BOARD_VENDOR_SEPOLICY_DIRS += device/xiaomi/andromeda-sepolicy/vendor/qcom/sm8150
